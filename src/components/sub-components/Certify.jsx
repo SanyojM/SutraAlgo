dcda544b953cf0e74ac90397b0c46397b49c8338
Humanshu <humanshujaglan@gmail.com>
2024-07-11 20:39:08 +0530

    error sorted

---
+++ src/components/sub-components/Certify.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Certify() {
+  return (
+    <div>Certify</div>
+  )
+}
---
+++ src/components/sub-components/Contest.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Contest() {
+  return (
+    <div>Contest</div>
+  )
+}
